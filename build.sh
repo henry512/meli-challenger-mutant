d9f78c02d7b52828faf76631f53f25895bd7ba0e
henry512 <34389493+henry512@users.noreply.github.com>
2022-01-15 17:44:45 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ git clone https://github.com/henry512/meli-challenger-mutant.git meli-mutant
 cd meli-mutant
 git checkout main
 git pull
-docker build -t henry512/api-meli-mutants:latest .
+docker build -t meli-mutants:latest .
 cd ..
